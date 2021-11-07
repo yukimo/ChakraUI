1de1b56c90889df4bc0799ea9177bcad03aa6e23
yukimo <yukimomiyamajp@yahoo.co.jp>
2021-11-07 21:43:48 +0900

    header_hamburger ハンバーガーメニュー実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Button, ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+
 import theme from "./theme/theme";
+import { Router } from "./router/Router";
 
 export default function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Button colorScheme="teal" size="lg" variant="solid" width="100%">
-        ボタン
-      </Button>
-      <p>aaaaaaaああああああ</p>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
--- src/compornents/pages/UserManegement.tsx
+++ src/compornents/pages/UserManegement.tsx
@@ -1,4 +1,4 @@
 import { memo, VFC } from "react";
-export const UseManegement: VFC = memo(() => {
+export const UserManegement: VFC = memo(() => {
   return <p>ユーザー管理ページです</p>;
 });
